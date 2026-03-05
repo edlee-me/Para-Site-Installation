12c99cc62a6a10b252c670e92a51dd6c870ce73c
Ed Lee <ed@edlee.me>
2026-03-05 12:16:11 +0800

    Remove credential

--- Night-Guard/check_models.py
+++ Night-Guard/check_models.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # 請記得填入你的真實 API Key
-client = genai.Client(api_key="AIzaSyDSId5qVigEBx8DsbRVAVPwtPfNIBh1RG4")
+client = genai.Client(api_key="")
 
 print("你的 API Key 支援以下模型：")
 for model in client.models.list():
